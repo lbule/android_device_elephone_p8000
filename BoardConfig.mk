82563c1909fc2d0519a01dafcb034e4feb7a31e6
Hans Cats <hanscats@gmail.com>
2015-08-05 13:55:55 +0200

    Updating all files to p8000

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,24 +1,49 @@
+LOCAL_PATH := device/elephone/p8000
+
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 -include vendor/alps/k05ts_a/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+#TARGET_NO_BOOTLOADER := true
+#TARGET_BOOTLOADER_BOARD_NAME := p8000
+#TARGET_OTA_ASSERT_DEVICE := k05ts_a
+
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6753
+#TARGET_BOARD_PLATFORM_GPU := Mali-T720
+
+# Architecture
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT_CPU := cortex-a53
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI_LIST := armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_32_BIT := armeabi-v7a,armeabi
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
+
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := k05ts_a
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := k05ts_a
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
 
+# partition info
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
@@ -28,4 +53,3 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/alps/k05ts_a/kernel
 
-BOARD_HAS_NO_SELECT_BUTTON := true
